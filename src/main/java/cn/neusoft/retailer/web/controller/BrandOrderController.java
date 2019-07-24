b986993f9f4b87cc6ef80380a827d3dd9f8eff94
Huston <1114197985@qq.com>
2019-07-24 09:13:23 +0800

    add html

--- src/main/java/cn/neusoft/retailer/web/controller/BrandOrderController.java
+++ src/main/java/cn/neusoft/retailer/web/controller/BrandOrderController.java
@@ -1,13 +1,14 @@
 package cn.neusoft.retailer.web.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
 /**
  * @author 胡献涛
  * @version 1.0
  * @date 2019/7/23 13:30
  */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class BrandOrderController {
