d2381397187f02ecf4a686640498902cbf217e0e
罗圣荣 <rreezii@outlook.com>
2019-07-26 11:56:33 +0800

    update
    Signed-off-by: 罗圣荣 <rreezii@outlook.com>

--- src/main/java/cn/neusoft/retailer/web/controller/UserController.java
+++ src/main/java/cn/neusoft/retailer/web/controller/UserController.java
@@ -15,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/main/java/cn/neusoft/retailer/web/tools/TokenValidate.java
+++ src/main/java/cn/neusoft/retailer/web/tools/TokenValidate.java
@@ -1,6 +1,9 @@
 package cn.neusoft.retailer.web.tools;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author 罗圣荣
@@ -9,8 +12,34 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class TokenValidate {
 
+    private static final String TOKEN_PREFIX = "gmdropship:vadidation:token:";
     @Autowired
-//    private StringRedisTemplate redisTemplate;
+    private StringRedisTemplate redisTemplate;
 
-    private static final String MANAGE_TOKEN_PREFIX = "gmdropship:manageuser:token:";
+    /**
+     * @描述: Redis保存token（30mins）
+     * @参数: [token, id]
+     * @返回值: void
+     * @创建人: 罗圣荣
+     * @创建时间: 2019/7/26
+     */
+    public void saveManageUser(String token, Integer id) {
+        redisTemplate.opsForValue().set(TOKEN_PREFIX + token, id + "", 30 * 60, TimeUnit.SECONDS);
+    }
+
+    /**
+     * @描述: 验证token并更新其生命周期
+     * @参数: [token]
+     * @返回值: java.lang.Long
+     * @创建人: 罗圣荣
+     * @创建时间: 2019/7/26
+     */
+    public Long findManageUserAndUpdate(String token) {
+        String userId = redisTemplate.opsForValue().get(TOKEN_PREFIX + token);
+        if (userId == null) {
+            return 0L;
+        }
+        saveManageUser(token, Integer.valueOf(userId));
+        return Long.parseLong(userId);
+    }
 }
